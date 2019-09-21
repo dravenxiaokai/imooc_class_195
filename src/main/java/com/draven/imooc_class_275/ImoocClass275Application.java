8e525c7df6702ede6ff90a50e1cd2da336cc3894
dravenxiaokai <jspankai@vip.qq.com>
2019-09-22 01:38:41 +0800

    add annotations

---
+++ src/main/java/com/draven/imooc_class_275/ImoocClass275Application.java
@@ -0,0 +1,13 @@
+package com.draven.imooc_class_275;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoocClass275Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImoocClass275Application.class, args);
+    }
+
+}
