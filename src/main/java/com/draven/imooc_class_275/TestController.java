3f7f46a58c8358ac252b0a3e0db21739e038b5ba
dravenxiaokai <jspankai@vip.qq.com>
2019-09-22 03:00:01 +0800

    代码并发模拟

---
+++ src/main/java/com/draven/imooc_class_275/TestController.java
@@ -0,0 +1,16 @@
+package com.draven.imooc_class_275;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@Slf4j
+public class TestController {
+    @RequestMapping("/test")
+    public String test(){
+        return "test";
+    }
+
+}
